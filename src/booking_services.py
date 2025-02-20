24b11f73df4cad156c482ffe7f3df270cd03e171
dennis pc <dennis.yip@symbiotech.com.au>
2025-02-20 23:23:57 +0800

    specific seat implemented

--- src/booking_services.py
+++ src/booking_services.py
@@ -1,3 +1,4 @@
+from src.constants import *
 from typing import List
 from src.constants import *
 from src.movie_and_seats import MovieAndSeats
@@ -32,51 +33,58 @@ class BookingServices:
 
         seats_need_to_book = num_of_tickets
         for current_searching_row in range(rows):
-            if seating_per_row % 2 == 0:
-                mid_seat_pos = []
-                mid_seat_pos.append(seating_per_row // 2 - 1)
-                mid_seat_pos.append(seating_per_row // 2)
-                mid_left_seat_pos = mid_seat_pos[0]
-                mid_right_seat_pos = mid_seat_pos[1]
-                if seating_map[current_searching_row][mid_left_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
-                    self.best_seats.append(
-                        (current_searching_row, mid_left_seat_pos))
-                    seats_need_to_book -= 1
-                if seating_map[current_searching_row][mid_right_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
-                    self.best_seats.append(
-                        (current_searching_row, mid_right_seat_pos))
-                    seats_need_to_book -= 1
-                seat_idx = 1
-                while seats_need_to_book > 0 and mid_seat_pos[1] + seat_idx < seating_per_row and mid_seat_pos[0] - seat_idx >= 0:
-                    left_seat_pos = mid_left_seat_pos - seat_idx
-                    right_seat_pos = mid_right_seat_pos + seat_idx
-                    if seating_map[current_searching_row][right_seat_pos] == AVAILABLE_SEAT_MARK:
-                        self.best_seats.append(
-                            (current_searching_row, right_seat_pos))
-                        seats_need_to_book -= 1
-                    if seating_map[current_searching_row][left_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
-                        self.best_seats.append(
-                            (current_searching_row, left_seat_pos))
-                        seats_need_to_book -= 1
-                    seat_idx += 1
-            else:
-                mid_seat_pos = seating_per_row // 2
-                # Try to fill the middle-most seats first
-                if seating_map[current_searching_row][mid_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
-                    self.best_seats.append(
-                        (current_searching_row, mid_seat_pos))
-                    seats_need_to_book -= 1
-                seat_idx = 1
-                while seats_need_to_book > 0 and mid_seat_pos + seat_idx <= seating_per_row and mid_seat_pos - seat_idx >= 0:
-                    if mid_seat_pos + seat_idx < seating_per_row and seating_map[current_searching_row][mid_seat_pos + seat_idx] == AVAILABLE_SEAT_MARK:
-                        self.best_seats.append(
-                            (current_searching_row, mid_seat_pos + seat_idx))
-                        seats_need_to_book -= 1
-                    if seating_map[current_searching_row][mid_seat_pos - seat_idx] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
-                        self.best_seats.append(
-                            (current_searching_row, mid_seat_pos - seat_idx))
-                        seats_need_to_book -= 1
-                    seat_idx += 1
+            seats_need_to_book = self.book_seats_in_row(seating_map, current_searching_row, seating_per_row, seats_need_to_book)
+            if seats_need_to_book == 0:
+                break
+        return self.best_seats
+    
+    def find_the_best_seats_specific_start_seat(self, start_seat:str):
+        """
+        - Starting from the specified position, fill up all empty seats in the same row all the way to the right of the cinema hall.
+        - When there are not enough seats available, it should overflow to the next row closer to the screen.
+        - Seat allocation for overflow follows the rules for default seat selection.
+
+        Args:
+            start_seat (str): Start seat position in the format of "A1"
+
+        Returns:
+            List[Tuple[int, int]]: list of coordinates of the best seats
+        """
+        self.best_seats = []
+        seating_map = self.seating_map
+        num_of_tickets = self.num_of_tickets
+        rows = len(seating_map)
+        seating_per_row = len(seating_map[0])
+
+        if num_of_tickets > self.movie_and_seats.available_seats:
+            print("Not enough seats available.")
+            return self.best_seats
+
+        row_idx = ord(start_seat[0].upper()) - LETTER_A_ASCII
+        specific_seat_pos = int(start_seat[1:]) - 1
+        if row_idx < 0 or row_idx >= rows or specific_seat_pos < 0 or specific_seat_pos >= seating_per_row:
+            print("Invalid start seat position.")
+            return self.best_seats
+
+        seats_need_to_book = num_of_tickets
+        if seating_map[row_idx][specific_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
+            self.best_seats.append(
+                (row_idx, specific_seat_pos))
+            seats_need_to_book -= 1
+        seat_idx = 1
+        while seats_need_to_book > 0 and specific_seat_pos + seat_idx < seating_per_row:
+            if seating_map[row_idx][specific_seat_pos + seat_idx] == AVAILABLE_SEAT_MARK:
+                self.best_seats.append(
+                    (row_idx, specific_seat_pos + seat_idx))
+                seats_need_to_book -= 1
+            seat_idx += 1
+        if seats_need_to_book == 0:
+            return self.best_seats
+        
+        for current_searching_row in range(row_idx+1, rows):
+            seats_need_to_book = self.book_seats_in_row(seating_map, current_searching_row, seating_per_row, seats_need_to_book)
+            if seats_need_to_book == 0:
+                break
         return self.best_seats
 
     def create_booking_id(self):
@@ -88,3 +96,44 @@ class BookingServices:
         self.movie_and_seats.seating_map = self.target_seating_map
         self.movie_and_seats.available_seats -= self.num_of_tickets
         self.movie_and_seats.booking_records[self.current_booking_id] = self.best_seats
+
+    def book_seats_in_row(self, seating_map: List[List[str]], current_searching_row: int, seating_per_row: int, seats_need_to_book: int) -> int:
+        """
+        Helper function to book available seats in a given row, starting from the middle.
+        """
+        if seating_per_row % 2 == 0:
+            mid_seat_pos = [seating_per_row // 2 - 1, seating_per_row // 2]
+            mid_left_seat_pos = mid_seat_pos[0]
+            mid_right_seat_pos = mid_seat_pos[1]
+            if seating_map[current_searching_row][mid_left_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
+                self.best_seats.append((current_searching_row, mid_left_seat_pos))
+                seats_need_to_book -= 1
+            if seating_map[current_searching_row][mid_right_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
+                self.best_seats.append((current_searching_row, mid_right_seat_pos))
+                seats_need_to_book -= 1
+            seat_idx = 1
+            while seats_need_to_book > 0 and mid_seat_pos[1] + seat_idx < seating_per_row and mid_seat_pos[0] - seat_idx >= 0:
+                left_seat_pos = mid_left_seat_pos - seat_idx
+                right_seat_pos = mid_right_seat_pos + seat_idx
+                if seating_map[current_searching_row][right_seat_pos] == AVAILABLE_SEAT_MARK:
+                    self.best_seats.append((current_searching_row, right_seat_pos))
+                    seats_need_to_book -= 1
+                if seating_map[current_searching_row][left_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
+                    self.best_seats.append((current_searching_row, left_seat_pos))
+                    seats_need_to_book -= 1
+                seat_idx += 1
+        else:
+            mid_seat_pos = seating_per_row // 2
+            if seating_map[current_searching_row][mid_seat_pos] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
+                self.best_seats.append((current_searching_row, mid_seat_pos))
+                seats_need_to_book -= 1
+            seat_idx = 1
+            while seats_need_to_book > 0 and mid_seat_pos + seat_idx < seating_per_row and mid_seat_pos - seat_idx >= 0:
+                if seating_map[current_searching_row][mid_seat_pos + seat_idx] == AVAILABLE_SEAT_MARK:
+                    self.best_seats.append((current_searching_row, mid_seat_pos + seat_idx))
+                    seats_need_to_book -= 1
+                if seating_map[current_searching_row][mid_seat_pos - seat_idx] == AVAILABLE_SEAT_MARK and seats_need_to_book > 0:
+                    self.best_seats.append((current_searching_row, mid_seat_pos - seat_idx))
+                    seats_need_to_book -= 1
+                seat_idx += 1
+        return seats_need_to_book
--- src/user_interface.py
+++ src/user_interface.py
@@ -4,6 +4,7 @@ from src.booking_services import BookingServices
 from typing import List
 import re
 
+
 class UserInterface:
     def __init__(self):
         self.movie_and_seats = None
@@ -17,7 +18,8 @@ class UserInterface:
                 title = movie_title_and_seats[0]
                 rows = int(movie_title_and_seats[1])
                 seats_per_row = int(movie_title_and_seats[2])
-                self.movie_and_seats = MovieAndSeats(title, rows, seats_per_row)
+                self.movie_and_seats = MovieAndSeats(
+                    title, rows, seats_per_row)
                 break
             except IndexError:
                 print("Please enter the correct format.")
@@ -27,7 +29,8 @@ class UserInterface:
     def main_menu(self):
         """ Main menu """
         movie_and_seats = self.movie_and_seats
-        print(f"Movie title: {movie_and_seats.title}, Rows: {movie_and_seats.rows}, Seats per row: {movie_and_seats.seats_per_row}")
+        print(
+            f"Movie title: {movie_and_seats.title}, Rows: {movie_and_seats.rows}, Seats per row: {movie_and_seats.seats_per_row}")
         while True:
             print("Welcome to Rocket Cinemas")
             print(
@@ -65,24 +68,46 @@ class UserInterface:
                 if num_of_tickets > movie_and_seats.available_seats:
                     print("Not enough seats available.")
                     continue
-                target_seating_map = [row[:] for row in movie_and_seats.seating_map]
-                booking_services = BookingServices(movie_and_seats, num_of_tickets, target_seating_map)
+                target_seating_map = [row[:]
+                                      for row in movie_and_seats.seating_map]
+                booking_services = BookingServices(
+                    movie_and_seats, num_of_tickets, target_seating_map)
                 best_seats = booking_services.find_the_best_seats()
                 for seat in best_seats:
                     row, col = seat
                     target_seating_map[row][col] = SELECTED_SEAT_MARK
                 booking_id = booking_services.create_booking_id()
-                print(f"Successfully reserved {num_of_tickets} {movie_and_seats.title} tickets.")
+                print(
+                    f"Successfully reserved {num_of_tickets} {movie_and_seats.title} tickets.")
                 print(f"Booking ID: {booking_id}")
                 self.display_seating_map(target_seating_map)
-                new_postion = input("Enter blank to accept seat selection, or enter a new seating position\n>")
+                new_postion = input(
+                    "Enter blank to accept seat selection, or enter a new seating position\n>")
                 if not new_postion:
                     booking_services.confirm_booking()
                     break
                 else:
-                    if not self.is_seat_input_string_valid(new_postion):
-                        break
-                    print("New position accepted.")
+                    while True:
+                        if not self.is_seat_input_string_valid(new_postion):
+                            break
+                        target_seating_map = [row[:]
+                                              for row in movie_and_seats.seating_map]
+                        booking_services = BookingServices(
+                            movie_and_seats, num_of_tickets, target_seating_map)
+                        best_seats = booking_services.find_the_best_seats_specific_start_seat(
+                            new_postion)
+                        for seat in best_seats:
+                            row, col = seat
+                            target_seating_map[row][col] = SELECTED_SEAT_MARK
+                        booking_id = booking_services.create_booking_id()
+                        self.display_seating_map(target_seating_map)
+                        new_postion = input(
+                            "Enter blank to accept seat selection, or enter a new seating position\n>")
+                        if not new_postion:
+                            booking_services.confirm_booking()
+                            print(f"Booking id: {booking_id} confirmed.")
+                            return
+
             except ValueError:
                 print("Please enter a valid number.")
 
@@ -91,7 +116,8 @@ class UserInterface:
         rows = len(seating_map)
         seats_per_row = len(seating_map[0])
         total_width = (seats_per_row*2)+1
-        screen_text_padding = SPACE_CHAR*((total_width - len(SCREEN_TEXT)) // 2)
+        screen_text_padding = SPACE_CHAR * \
+            ((total_width - len(SCREEN_TEXT)) // 2)
         print(f"{screen_text_padding}{SCREEN_TEXT}{screen_text_padding}")
         print("-" * total_width)
         for i in range(rows-1, -1, -1):
@@ -99,12 +125,13 @@ class UserInterface:
             print(f"{row_letter} {SPACE_CHAR.join(seating_map[i])}")
         number_str = SPACE_CHAR.join(str(i+1) for i in range(seats_per_row))
         print(f"  {number_str}")
-    
+
     def check_bookings(self):
         """ Check bookings """
         movie_and_seats = self.movie_and_seats
         while True:
-            booking_id = input("Enter booking ID to check booking details, or enter blank to go back to the main menu:\n> ")
+            booking_id = input(
+                "Enter booking ID to check booking details, or enter blank to go back to the main menu:\n> ")
             if not booking_id:
                 return
             if not re.match(r'^HKG[0-9]{4}$', booking_id):
@@ -119,7 +146,7 @@ class UserInterface:
                 row, col = seat
                 seating_map[row][col] = SELECTED_SEAT_MARK
             self.display_seating_map(seating_map)
-    
+
     def is_seat_input_string_valid(self, seat_input: str):
         """
         Validate seat input string
